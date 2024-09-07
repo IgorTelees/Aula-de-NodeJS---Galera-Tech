a81e501f6bae8c1b8e40a6cedb8f01f0c5e687b2
IgorTelees <igortteeles@gmail.com>
2024-09-07 17:31:39 -0300

    Controle de Notas

--- src/dao/disciplina.dao.js
+++ src/dao/disciplina.dao.js
@@ -7,3 +7,43 @@ export const selectDisciplinas = async (page, limit) => {
   
   return result.recordset;
 };
+
+
+
+export const selectDisciplinasById = async (id) => {
+  const result = await db.query`SELECT * FROM Disciplinas WHERE ID = ${id}`;
+  return result.recordset[0];
+};
+
+// Selecionar disciplinas com base no nome, com paginação
+export const selectDisciplinasByNome = async (nome, page, limit) => {
+  const offset = (page - 1) * limit;
+  const result = await db.query`SELECT * FROM Disciplinas WHERE UPPER(Nome) 
+  LIKE UPPER(${`%${nome}%`}) ORDER BY ID OFFSET ${Number(offset)} ROWS
+    FETCH NEXT ${Number(limit)} ROWS ONLY`;
+  return result.recordset;
+};
+
+// Inserir uma nova disciplina
+export const insertDisciplina = async (disciplina) => {
+  const result = await db.query`
+    INSERT INTO Disciplinas (Nome, ProfessorID) VALUES (${disciplina.nome}, ${disciplina.professorId})
+  `;
+  return result.recordset;
+};
+
+// Atualizar uma disciplina pelo ID
+export const updateDisciplina = async (id, disciplina) => {
+  const result = await db.query`
+    UPDATE Disciplinas SET Nome = ${disciplina.nome}, ProfessorID = ${disciplina.professorId} WHERE ID = ${id}
+  `;
+  return result.recordset;
+};
+
+// Deletar uma disciplina pelo ID
+export const deleteDisciplina = async (id) => {
+  const result = await db.query`
+    DELETE FROM Disciplinas WHERE ID = ${id}
+  `;
+  return result.recordset;
+};
